9ea5d52bc694641f767b9d366ce51a4e16d92afd
Cloud User <ec2-user@ip-172-31-31-219.us-west-2.compute.internal>
2018-01-29 04:28:51 +0000

    runOpt_params.m set up for aws

--- Optimization_software/runOpt_params.m
+++ Optimization_software/runOpt_params.m
@@ -1,5 +1,6 @@
-%lumerical = '/data/lumerical/fdtd/bin/CAD';
+% AWS example AMI settings
+lumerical = '/opt/lumerical/fdtd/bin/fdtd-solutions';
 %Mac OS
-lumerical='/Applications/Lumerical/FDTD\ Solutions/FDTD\ Solutions.app/Contents/MacOS/fdtd-solutions'
+%lumerical='/Applications/Lumerical/FDTD\ Solutions/FDTD\ Solutions.app/Contents/MacOS/fdtd-solutions'
 %lumerical = '"C:\Program Files\Lumerical\FDTD\bin\fdtd-solutions.exe"';
 queueName = 'local'; % either 'local' or a pbs_server queue
